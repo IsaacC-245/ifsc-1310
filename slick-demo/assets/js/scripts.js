a1cf86d280eb488a69cc184034cabb66c44e2668
IsaacC-245 <96172767+IsaacC-245@users.noreply.github.com>
2022-05-11 14:30:45 -0500

    + js to header + fix img folder naming

--- slick-demo/assets/js/scripts.js
+++ slick-demo/assets/js/scripts.js
@@ -15,3 +15,19 @@ $(document).ready(function() {
 
 
 });
+
+$(document).ready(function() {
+
+  $('.fade').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    slide: 'div',
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+
+
+});
